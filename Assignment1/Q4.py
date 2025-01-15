465e2740c5e40f74fd829f71ecc5a8d871c6e1bf
Navnidhi-gandhi07 <nnavnidhi_be23@thapar.edu>
2025-01-15 22:33:10 +0530

    Update Q4.py

--- Assignment1/Q4.py
+++ Assignment1/Q4.py
@@ -1,1 +1,4 @@
-
+#4. Loops
+#4.1 Write a program to print numbers from 1 to 10 using a for loop.
+#4.2 Write a program to print numbers from 1 to 10 using a while loop.
+#4.3 Write a program to calculate the sum of numbers from 1 to 100 using a loop.
