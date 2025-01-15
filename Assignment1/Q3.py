d0e73e3cf87182be17d603460941acbcf78e58c8
Navnidhi-gandhi07 <nnavnidhi_be23@thapar.edu>
2025-01-15 11:30:27 +0530

    Update Q3.py

--- Assignment1/Q3.py
+++ Assignment1/Q3.py
@@ -1,1 +1,4 @@
-
+#3. If-Else Statements
+#3.1 Write a Python program to check if a number is positive, negative, or zero
+#using an if-else statement.
+#3.2 Write a program to check if a given number is odd or even.
