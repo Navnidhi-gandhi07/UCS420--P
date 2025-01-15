034e2ea59f376899d49afaa468b5de075108ee0d
Navnidhi-gandhi07 <nnavnidhi_be23@thapar.edu>
2025-01-15 11:25:43 +0530

    Update and rename Q2 to Q2.py

---
+++ Assignment1/Q2.py
@@ -0,0 +1,24 @@
+#2. Add Numbers and Concatenate Strings
+#2.1 Write a program to add two numbers and print the result.
+#2.2 Write a program to concatenate two strings and print the result.
+#2.3 Write a program to concatenate a string and a number and print the result.
+
+
+#2.1 Write a program to add two numbers and print the result.
+a=int(input("the value of a:"))
+b=int(input("the value of b:"))
+c=a+b
+print("the value of a+b:",c)
+
+
+#2.2 Write a program to concatenate two strings and print the result.
+a="hello , my name "
+b="is Navnidhi"
+print("the concatinated string :",a+" "+b)
+
+
+#2.3 Write a program to concatenate a string and a number and print the result.
+code=23
+c="the code for the subject is"
+d=c+" " +str(code)
+print(d)
