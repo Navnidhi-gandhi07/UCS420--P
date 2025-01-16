9a10ebe8a5358b88546d441db1f200e9677432de
Navnidhi <nnavnidhi_be23@thapar.edu>
2025-01-16 12:29:30 +0530

    Update Q8.py

--- Assignment1/Q8.py
+++ Assignment1/Q8.py
@@ -3,3 +3,28 @@
 #8.2 Write a program to handle invalid input (e.g., when the user enters a string
 #instead of a number).
 #8.3 Write a program to demonstrate the use of finally in exception handling.
+
+
+#8.1 Write a program to handle division by zero using a try-except block.
+try:
+    n1 = int(input("enter the  numerator: "))
+    n2 = int(input("enter the denominator: "))
+    division= n1 / n2
+    print("Result of division:", division)
+except divisionbyzeroerror:
+    print("Error: Division by zero is not allowed.")
+
+
+#8.2 Write a program to handle invalid input (e.g., when the user enters a string
+#instead of a number).
+#8.3 Write a program to demonstrate the use of finally in exception handling.
+try:
+    number = int(input("Enter the number: "))
+    print("entered the number", number)
+except invalidinput:
+    print("Error: Invalid input")
+  finally:
+    print("Execution of the program is completed")
+  
+
+  
