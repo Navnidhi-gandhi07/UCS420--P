d4ef0f3ef05f82a0d7eb24f6d81ee4b2a9ae95e0
Navnidhi <nnavnidhi_be23@thapar.edu>
2025-01-16 12:07:18 +0530

    Update Q6.py

--- Assignment1/Q6.py
+++ Assignment1/Q6.py
@@ -2,3 +2,31 @@
 #6.1 Write a program to count the number of vowels in a given string.
 #6.2 Write a program to reverse a string and print it.
 #6.3 Write a program to check if a string is a palindrome.
+
+
+#6.1 Write a program to count the number of vowels in a given string.
+string1 = input("Enter a string: ")
+vowels = "aeiouAEIOU"
+i = 0
+for j in string1:
+    if j in vowels:
+        i += 1
+print("Number of vowels ", i)
+
+
+#6.2 Write a program to reverse a string and print it.
+string1 = input("Enter a string: ")
+reversedstring = ''.join(reversed(string1))
+print("Reversed string:", reversedstring)
+
+
+#6.3 Write a program to check if a string is a palindrome.
+string1 = input("Enter a string: ")
+updatedstring = string1.replace(" ", "").lower()
+if updatedstring == updatedstring[::-1]:
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
+
+
+
