7260cd7b9712167be3fbed10b19dd35801e141c4
Navnidhi <nnavnidhi_be23@thapar.edu>
2025-01-16 11:30:41 +0530

    Update Q7.py

--- Assignment1/Q7.py
+++ Assignment1/Q7.py
@@ -1,1 +1,6 @@
-
+#7. File Handling
+#7.1 Write a program to create a text file, write some text into it, and then read and
+#print the content.
+#7.2 Write a program to append text to an existing file and print the updated
+#content.
+#7.3 Write a program to count the number of lines in a text file.
