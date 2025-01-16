81e4ff98e187b8b1f96db6d56f078a4007c4d8f9
Navnidhi <nnavnidhi_be23@thapar.edu>
2025-01-16 22:47:39 +0530

    Update Q1.py

--- Assignment2/Q1.py
+++ Assignment2/Q1.py
@@ -1,1 +1,5 @@
-
+#1. Create a List L that is defined as= [10, 20, 30, 40, 50, 60, 70, 80].
+#i. WAP to add 200 and 300 to L.
+#ii. WAP to remove 10 and 30 from L.
+#iii. WAP to sort L in ascending order.
+#iv. WAP to sort L in descending order.
