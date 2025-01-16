e266c3c96a4339e6e0700163dfb3aea15940b9ba
Navnidhi <nnavnidhi_be23@thapar.edu>
2025-01-16 12:35:23 +0530

    Update Q10.py

--- Assignment1/Q10.py
+++ Assignment1/Q10.py
@@ -1,1 +1,5 @@
-
+#10. Command Line Arguments
+#10.1 Write a program to accept two numbers as command-line arguments, add
+#them, and print the result.
+#10.2 Write a program to accept a string as a command-line argument and print its
+#length.
