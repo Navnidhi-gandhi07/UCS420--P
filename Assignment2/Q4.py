0de5645b1b2f8d6513cbb86d7eeab78a7c91c43f
Navnidhi <nnavnidhi_be23@thapar.edu>
2025-01-16 22:50:42 +0530

    Create Q4.py

---
+++ Assignment2/Q4.py
@@ -0,0 +1,10 @@
+#Consider the following two sets, A and B, represenƟng scores of two teams in mulƟple
+#matches. A = {34, 56, 78, 90} and B = {78, 45, 90, 23}
+#WAP to perform the following operaƟons using set funcƟons:
+#i. Find the unique scores achieved by both teams (union of sets).
+#ii. IdenƟfy the scores that are common to both teams (intersecƟon of sets).
+#iii. Find the scores that are exclusive to each team (symmetric difference).
+#iv. Check if the scores of team A are a subset of team B, and if team B's scores are
+#a superset of team A.
+#v. Remove a specific score X (input by the user) from set A if it exists. If not, print
+#a message saying it is not present.
