e11010db33016b84e2dc0fa46bb261d35c63ee7c
Navnidhi <nnavnidhi_be23@thapar.edu>
2025-01-16 22:48:37 +0530

    Create Q2.py

---
+++ Assignment2/Q2.py
@@ -0,0 +1,7 @@
+#2. Create a tuple of marks scored as scores = (45, 89.5, 76, 45.4, 89, 92, 58, 45) and
+#perform the following operations using tuple functions:
+#i. Identify the highest score and its index in the tuple.
+#ii. Find the lowest score and count how many times it appears.
+#iii. Reverse the tuple and return it as a list.
+#iv. Check if a specific score ‘76’ (input by the user) is present in the tuple and
+#print its first occurrence index, or a message saying it’s not present.
