72da759a2de21d30cd9a7a1ca0c5ebd0fe8d26d2
Navnidhi <nnavnidhi_be23@thapar.edu>
2025-01-16 11:31:29 +0530

    Update Q8.py

--- Assignment1/Q8.py
+++ Assignment1/Q8.py
@@ -1,1 +1,5 @@
-
+#8. Exception Handling
+#8.1 Write a program to handle division by zero using a try-except block.
+#8.2 Write a program to handle invalid input (e.g., when the user enters a string
+#instead of a number).
+#8.3 Write a program to demonstrate the use of finally in exception handling.
