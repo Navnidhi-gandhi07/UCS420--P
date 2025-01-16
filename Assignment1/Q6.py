6a69bf91f9462266f7bb59921cbdf989db82474a
Navnidhi <nnavnidhi_be23@thapar.edu>
2025-01-16 11:28:01 +0530

    Update and rename Q6 to Q6.py

---
+++ Assignment1/Q6.py
@@ -0,0 +1,4 @@
+#6. Strings
+#6.1 Write a program to count the number of vowels in a given string.
+#6.2 Write a program to reverse a string and print it.
+#6.3 Write a program to check if a string is a palindrome.
