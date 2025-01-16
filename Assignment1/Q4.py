a6010b9370acfe2ca06a5d8bb7d237aa807b67c9
Navnidhi <nnavnidhi_be23@thapar.edu>
2025-01-16 11:00:37 +0530

    Update Q4.py

--- Assignment1/Q4.py
+++ Assignment1/Q4.py
@@ -2,3 +2,24 @@
 #4.1 Write a program to print numbers from 1 to 10 using a for loop.
 #4.2 Write a program to print numbers from 1 to 10 using a while loop.
 #4.3 Write a program to calculate the sum of numbers from 1 to 100 using a loop.
+
+
+#4.1 Write a program to print numbers from 1 to 10 using a for loop.
+n=int(input("enter the value of n "))
+for i in range(1,n+1):
+   print(i,end=" ")
+
+
+#4.2 Write a program to print numbers from 1 to 10 using a while loop.
+i=1
+while i<=10:
+    print(i,end=" ")
+    i=i+1
+
+
+#4.3 Write a program to calculate the sum of numbers from 1 to 100 using a loop.
+sum=0
+for i in range(1,101):
+    sum=sum+i
+    
+print(sum)
