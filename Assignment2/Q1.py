6d412e225adc47d5ac74c9c5aad218c7ed4531ae
Navnidhi <nnavnidhi_be23@thapar.edu>
2025-01-17 10:18:21 +0530

    Update Q1.py

--- Assignment2/Q1.py
+++ Assignment2/Q1.py
@@ -3,3 +3,31 @@
 #ii. WAP to remove 10 and 30 from L.
 #iii. WAP to sort L in ascending order.
 #iv. WAP to sort L in descending order.
+
+L=[10,20,30,40,50,60,70,80]
+
+
+#i. WAP to add 200 and 300 to L.
+L.append(200)
+L.append(300)
+print("the list after adding 200 & 300:")
+print(L)
+
+
+#ii. WAP to remove 10 and 30 from L.
+L.remove(10)
+L.remove(30)
+print("the list after removing 10 &30:")
+print(L)
+
+
+#iii. WAP to sort L in ascending order.
+L.sort()
+print("the list after sorting in ascending oder:")
+print(L)
+
+
+#iv. WAP to sort L in descending order.
+L.reverse()
+print("the list after sorting in descending order:")
+print(L)
