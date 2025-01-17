ec4e9f597104ad45d122115b04d9e5118e3973bb
Navnidhi <nnavnidhi_be23@thapar.edu>
2025-01-17 12:22:01 +0530

    Update Q5.py

--- Assignment2/Q5.py
+++ Assignment2/Q5.py
@@ -1,1 +1,11 @@
 #Write a program to rename a key city to a locaƟon in the following dicƟonary.
+dictionary={ "name":"navnidhi", "age":"19","salary":"1,00,000","city":"Ludhiana"}
+
+updatedkey = "location"
+oldkey = "city"
+
+if oldkey in dictionary:
+    dictionary[updatedkey] = dictionary.pop(oldkey)
+    print(f"Updated dictionary: {dictionary}")
+else:
+    print(f"Key  not found in the dictionary.")
